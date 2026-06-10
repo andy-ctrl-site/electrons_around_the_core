9cc1ba2c471279b9c4f5cc1a23492e8cbd5edc3d
Andy <if25b088@technikum-wien.at>
2026-06-10 17:43:38 +0200

    Step 2 (Andy): failling test 1

---
+++ spec/electrons_aroud_core_spec.js
@@ -0,0 +1,7 @@
+describe("electronsAroundCores", function() {
+
+    it("should return 6 for [1, 2, 3, 4, 5]", function() {
+        expect(electronsAroundCores([1, 2, 3, 4, 5])).toEqual(6);
+    });
+
+});
