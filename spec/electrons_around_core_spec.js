51c4c489596645011b2c5b8624b227fffee5ee1b
Michael Lin <michaelson.at@gmail.com>
2026-06-10 18:12:25 +0200

    Step 2.3 (Michael): added failing test 2

--- spec/electrons_around_core_spec.js
+++ spec/electrons_around_core_spec.js
@@ -9,4 +9,8 @@ describe("electronsAroundCores", function() {
         expect(electronsAroundCores([1, 2, 3, 4, 5])).toEqual(6);
     });
 
+    it("should return 4 for [2, 2, 3, 3]", function() {
+        expect(electronsAroundCores([2, 2, 3, 3])).toEqual(4);
+    });
+
 });
